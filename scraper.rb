7da9b698417a4451b51fce3ef58e2d2370163edd
Tony Bowden <tony@tmtm.com>
2016-11-26 10:25:09 +0000

    Replace WDQ with SPARQL

--- scraper.rb
+++ scraper.rb
@@ -3,6 +3,7 @@
 
 require 'wikidata/fetcher'
 
-ids = EveryPolitician::Wikidata.wdq('claim[39:21290853]')
+sparq = 'SELECT ?item WHERE { ?item wdt:P39 wd:Q21290853 . }'
+ids = EveryPolitician::Wikidata.sparql(sparq)
 raise "No ids" if ids.empty?
 EveryPolitician::Wikidata.scrape_wikidata(ids: ids)
